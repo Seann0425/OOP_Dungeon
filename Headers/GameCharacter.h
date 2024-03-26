fa882a7e0a7130e39534c56d69dbe98a165381eb
Seann <seanyu0425@gmail.com>
2024-03-26 09:20:32 +0800

    some GameCharacter implementation

---
+++ Headers/GameCharacter.h
@@ -0,0 +1,8 @@
+#ifndef GC
+#define GC
+#include <bits/stdc++.h>
+
+#include "Object.h"
+using namespace std;
+
+#endif
--- Sources/main.cpp
+++ Sources/main.cpp
@@ -1,4 +1,6 @@
 #include <bits/stdc++.h>
+
+#include "../Headers/Item.h"
 using namespace std;
 
 int main() {
