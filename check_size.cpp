566bc0813acb7f14beb498cca4bec4c922c94828
wytsai7660 <wytsai7660@gmail.com>
2024-04-19 10:50:57 +0800

    fix powershell compatibility

--- check_size.cpp
+++ check_size.cpp
@@ -67,6 +67,10 @@ int main() {
         if (std::toupper(getch()) == 'Q') break;
     }
     endwin();
+#ifdef _WIN32
+    std::system("cls");
+#else
     std::system("clear");
+#endif
     return 0;
 }
